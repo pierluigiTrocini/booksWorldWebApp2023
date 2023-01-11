5233c146ccdf1dec78278c372113f83115475b17
pierluigi <pierluigitrc@gmail.com>
2023-01-11 18:14:44 +0100

    Inizializzazione repository

---
+++ books_world/src/main/java/it/books_world/books_world/BooksWorldApplication.java
@@ -0,0 +1,13 @@
+package it.books_world.books_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksWorldApplication.class, args);
+	}
+
+}
---
+++ books_world/src/test/java/it/books_world/books_world/BooksWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package it.books_world.books_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
