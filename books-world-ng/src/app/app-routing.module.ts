d0248301688b7f8956bf59050c4ad4cb92e99823
Astorix10 <alexdoesnt@gmail.com>
2023-02-03 15:54:27 +0100

    icona profilo e routing

--- books-world-ng/src/app/app-routing.module.ts
+++ books-world-ng/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { BooksCardComponent } from './books-card/books-card.component';
 import { DettagliLibriComponent } from './dettagli-libri/dettagli-libri.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { ProfiloComponent } from './profilo/profilo.component';
 
 const routes: Routes = [
   {path: '#', component: MenuBarComponent},
   {path: 'book/:ISBN', component: DettagliLibriComponent},
-  {path: 'profilo'}
+  {path: 'profilo',component:ProfiloComponent}
 ];
 
 @NgModule({
